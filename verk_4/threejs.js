ff67cd2988d72a8f1d83c0719420e9cba692ecdc
Emilb05 <89970307+Emilb05@users.noreply.github.com>
2024-04-18 22:58:46 +0000

    Update threejs.js

--- verk_4/threejs.js
+++ verk_4/threejs.js
@@ -1,72 +1,191 @@
-import * as THREE from "three";
+const img  = document.getElementById('bitmap');
+// Ensure the image is loaded and ready for use
+var imgBitmap = null
+createImageBitmap(img).then(x=>{imgBitmap = x});
 
-const scene = new THREE.Scene();
+//   each frame send to socket.
 
-// The cube will have a different color on each side.
-const materials = [
-  new THREE.MeshBasicMaterial({color: 0xff0000}),
-  new THREE.MeshBasicMaterial({color: 0x0000ff}),
-  new THREE.MeshBasicMaterial({color: 0x00ff00}),
-  new THREE.MeshBasicMaterial({color: 0xff00ff}),
-  new THREE.MeshBasicMaterial({color: 0x00ffff}),
-  new THREE.MeshBasicMaterial({color: 0xffff00})
-];
+let clock = new THREE.Clock()
 
-// Create the cube and add it to the demo scene.
-const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(0.2, 0.2, 0.2), materials);
-cube.position.set(1, 1, 1);
-scene.add(cube);
+// standard webxr scene
 
-// Set up the WebGLRenderer, which handles rendering to the session's base layer.
-const renderer = new THREE.WebGLRenderer({
-    alpha: true,
-    preserveDrawingBuffer: true,
-    canvas: canvas,
-    context: gl
-  });
-  renderer.autoClear = false;
+function xwwwform(jsonObject){
+	return Object.keys(jsonObject).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(jsonObject[key])).join('&');
+}
+
+let camera, scene, renderer, xrRefSpace, gl;
+
+scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const material = new THREE.MeshStandardMaterial( {color: 0x00ff00} );
+desktopCube = new THREE.Mesh( geometry, material );
+scene.add( desktopCube );
+desktopCube.position.z -= 0.5
+
+const geometry1 = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );
+const material1 = new THREE.MeshStandardMaterial( {color: 0xcc6600} );
+earthCube = new THREE.Mesh( geometry1, material1 );
+scene.add( earthCube );
+
+
+var ambient = new THREE.AmbientLight( 0x222222 );
+scene.add( ambient );
+var directionalLight = new THREE.DirectionalLight( 0xdddddd, 1.5 );
+directionalLight.position.set( 0.9, 1, 0.6 ).normalize();
+scene.add( directionalLight );
+var directionalLight2 = new THREE.DirectionalLight( 0xdddddd, 1 );
+directionalLight2.position.set( -0.9, -1, -0.4 ).normalize();
+scene.add( directionalLight2 );
+
+camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 20000 );
+renderer = new THREE.WebGLRenderer({antialias: true,alpha:true });
+renderer.setPixelRatio( window.devicePixelRatio );
+camera.aspect = window.innerWidth / window.innerHeight;
+renderer.setSize(window.innerWidth, window.innerHeight );
+camera.updateProjectionMatrix();
+document.body.appendChild( renderer.domElement );	
+renderer.xr.enabled = true;
+
+function init() {
+	window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function getXRSessionInit( mode, options) {
+  	if ( options && options.referenceSpaceType ) {
+  		renderer.xr.setReferenceSpaceType( options.referenceSpaceType );
+  	}
+  	var space = (options || {}).referenceSpaceType || 'local-floor';
+  	var sessionInit = (options && options.sessionInit) || {};
   
-  // The API directly updates the camera matrices.
-  // Disable matrix auto updates so three.js doesn't attempt
-  // to handle the matrices independently.
-  const camera = new THREE.PerspectiveCamera();
-  camera.matrixAutoUpdate = false;
+  	// Nothing to do for default features.
+  	if ( space == 'viewer' )
+  		return sessionInit;
+  	if ( space == 'local' && mode.startsWith('immersive' ) )
+  		return sessionInit;
+  
+  	// If the user already specified the space as an optional or required feature, don't do anything.
+  	if ( sessionInit.optionalFeatures && sessionInit.optionalFeatures.includes(space) )
+  		return sessionInit;
+  	if ( sessionInit.requiredFeatures && sessionInit.requiredFeatures.includes(space) )
+  		return sessionInit;
+  
+  	var newInit = Object.assign( {}, sessionInit );
+  	newInit.requiredFeatures = [ space ];
+  	if ( sessionInit.requiredFeatures ) {
+  		newInit.requiredFeatures = newInit.requiredFeatures.concat( sessionInit.requiredFeatures );
+  	}
+  	return newInit;
+   }
 
-  // Initialize a WebXR session using "immersive-ar".
-const session = await navigator.xr.requestSession("immersive-ar");
-session.updateRenderState({
-  baseLayer: new XRWebGLLayer(session, gl)
-});
+function AR(){
+	var currentSession = null;
+	function onSessionStarted( session ) {
+		session.addEventListener( 'end', onSessionEnded );
+		renderer.xr.setSession( session );
+		gl = renderer.getContext()
+		button.style.display = 'none';
+		button.textContent = 'EXIT AR';
+		currentSession = session;
+		session.requestReferenceSpace('local').then((refSpace) => {
+          xrRefSpace = refSpace;
+          session.requestAnimationFrame(onXRFrame);
+        });
+	}
+	function onSessionEnded( /*event*/ ) {
+		currentSession.removeEventListener( 'end', onSessionEnded );
+		renderer.xr.setSession( null );
+		button.textContent = 'ENTER AR' ;
+		currentSession = null;
+	}
+	if ( currentSession === null ) {
+		
+        let options = {
+            requiredFeatures: ['dom-overlay','image-tracking'],
+            trackedImages: [
+              {
+                image: imgBitmap,
+                widthInMeters: 0.2
+              }
+            ],
+            domOverlay: { root: document.body }
+        };
+		var sessionInit = getXRSessionInit( 'immersive-ar', {
+			mode: 'immersive-ar',
+			referenceSpaceType: 'local', // 'local-floor'
+			sessionInit: options
+		});
+		navigator.xr.requestSession( 'immersive-ar', sessionInit ).then( onSessionStarted );
+	} else {
+		currentSession.end();
+	}
+	renderer.xr.addEventListener('sessionstart',
+		function(ev) {
+			console.log('sessionstart', ev);
+			document.body.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+			renderer.domElement.style.display = 'none';
+		});
+	renderer.xr.addEventListener('sessionend',
+		function(ev) {
+			console.log('sessionend', ev);
+			document.body.style.backgroundColor = '';
+			renderer.domElement.style.display = '';
+		});
+}
 
-// A 'local' reference space has a native origin that is located
-// near the viewer's position at the time the session was created.
-const referenceSpace = await session.requestReferenceSpace('local');
-
-// Create a render loop that allows us to draw on the AR view.
-const onXRFrame = (time, frame) => {
-    // Queue up the next draw request.
+function onXRFrame(t, frame) {
+    const session = frame.session;
     session.requestAnimationFrame(onXRFrame);
-  
-    // Bind the graphics framebuffer to the baseLayer's framebuffer
-    gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer.framebuffer)
-  
-    // Retrieve the pose of the device.
-    // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
-    const pose = frame.getViewerPose(referenceSpace);
-    if (pose) {
-      // In mobile AR, we only have one view.
-      const view = pose.views[0];
-  
-      const viewport = session.renderState.baseLayer.getViewport(view);
-      renderer.setSize(viewport.width, viewport.height)
-  
-      // Use the view's transform matrix and projection matrix to configure the THREE.camera.
-      camera.matrix.fromArray(view.transform.matrix)
-      camera.projectionMatrix.fromArray(view.projectionMatrix);
-      camera.updateMatrixWorld(true);
-  
-      // Render the scene with THREE.WebGLRenderer.
-      renderer.render(scene, camera)
+    const baseLayer = session.renderState.baseLayer;
+    const pose = frame.getViewerPose(xrRefSpace);
+	render()
+	if (pose) {
+		for (const view of pose.views) {
+            const viewport = baseLayer.getViewport(view);
+            gl.viewport(viewport.x, viewport.y,
+                        viewport.width, viewport.height);
+			const results = frame.getImageTrackingResults();
+			for (const result of results) {
+			  // The result's index is the image's position in the trackedImages array specified at session creation
+			  const imageIndex = result.index;
+			
+			  // Get the pose of the image relative to a reference space.
+			  const pose1 = frame.getPose(result.imageSpace, xrRefSpace);
+			  pos = pose1.transform.position
+			  quat = pose1.transform.orientation
+
+			  earthCube.position.copy( pos.toJSON())
+			  earthCube.quaternion.copy(quat.toJSON())
+			  const state = result.trackingState;
+			
+			  if (state == "tracked") {
+				// HighlightImage(imageIndex, pose1);
+			  } else if (state == "emulated") {
+				// FadeImage(imageIndex, pose1);
+			  }
+			}
+
+
+        }
     }
-  }
-  session.requestAnimationFrame(onXRFrame);
+
+}
+init()
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+render()
+function render() {
+	renderer.render( scene, camera );
+}
+
+
+var button = document.createElement( 'button' );
+button.id = 'ArButton'
+button.textContent = 'ENTER AR' ;
+button.style.cssText+= `position: absolute;top:80%;left:40%;width:20%;height:2rem;`;
+    
+document.body.appendChild(button)
+document.getElementById('ArButton').addEventListener('click',x=>AR())
